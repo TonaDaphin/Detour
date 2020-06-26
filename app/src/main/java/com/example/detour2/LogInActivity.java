08c4656d003fbcebef8cfffea88e57f5defb1229
Tona <tonadaphine77@gmail.com>
2020-06-26 16:48:59 +0200

    Constrait layouts

---
+++ app/src/main/java/com/example/detour2/LogInActivity.java
@@ -0,0 +1,14 @@
+package com.example.detour2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
